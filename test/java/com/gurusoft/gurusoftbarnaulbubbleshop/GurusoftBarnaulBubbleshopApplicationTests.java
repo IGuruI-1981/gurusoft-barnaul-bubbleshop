4e2a6b50cb961ea3fc472f05a51f5fa5da8f2b7e
Viktor <viktor.ihtiyarov@gmail.com>
2023-03-25 11:38:06 +0700

    Initial commit

---
+++ test/java/com/gurusoft/gurusoftbarnaulbubbleshop/GurusoftBarnaulBubbleshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurusoft.gurusoftbarnaulbubbleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GurusoftBarnaulBubbleshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
